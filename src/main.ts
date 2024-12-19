75a609363d0d3ca312777e3bc4785cb685ee9b15
gablsl <gabrielsousadeveloper@gmail.com>
2024-12-19 15:14:23 -0300

    feat: add aws s3 to storage movies posters

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,14 @@ import { NotFoundFilter } from './common/filters/not-found.filter';
 import { AlreadyExistsFilter } from './common/filters/already-exists.filter';
 import { ValidationPipe } from '@nestjs/common';
 import * as bodyParser from 'body-parser';
+import { ValidationPipe } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(bodyParser.json({ limit: '10mb' }));
+  app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
   app.enableCors();
 
