2ea9722f3a15d394f568deb0b57f119fa2a1794a
gablsl <gabrielsousadeveloper@gmail.com>
2024-12-19 16:24:49 -0300

    refactor: incorrect field name

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -2,7 +2,7 @@ export class CreateMovieDto {
   imgUrl: string;
   name: string;
   synopsis: string;
-  episodies?: number;
+  episodes?: number;
   year: number;
   genres: string[];
   director: string;
